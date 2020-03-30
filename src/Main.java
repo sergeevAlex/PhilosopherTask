ce53392adc3c5cd15431045617e216a15f7c5c31
Alexey Sergeev <alexsergeev@i.ua>
2020-03-31 01:27:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import java.util.concurrent.Semaphore;
 public class Main {
 
     public static void main(String[] args) {
-
         Semaphore sm = new Semaphore(2);
-
-        for(int i =0;i < 5; i++){
+        
+        for(int i =0; i < 5; i++) {
             new Thread(new Philosopher(sm,i)).start();
         }
-
     }
 }
